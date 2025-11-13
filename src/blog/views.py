ddbc153dd55052bd88aca899573b6ecf1fa73736
iamurtazo <murtazo207@inha.edu>
2025-11-13 21:10:14 +0900

    Refactor blog templates and views to enhance structure and styling

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'John Smith',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2024'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2024'
+    }
+]
+
 # Create your views here.
 def home(request):
     context = {
         'title': 'Home Page',
+        'posts': posts
     }
 
     return render(request, 'blog/home.html', context)
